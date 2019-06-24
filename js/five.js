fb8f522179499a936c0ed598b24c2865ef5f6e72
egameland <50410918+egameland@users.noreply.github.com>
2019-06-24 22:37:29 +0900

    Add files via upload

--- js/five.js
+++ js/five.js
@@ -10,11 +10,11 @@
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
+('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
